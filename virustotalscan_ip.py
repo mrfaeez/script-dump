a88bdea8e3c33f9b127baa1d7cdc47589d9bc1f7
Faeez <47419302+mrfaeez@users.noreply.github.com>
2022-10-18 17:10:27 +0800

    refine and add feature to upload from txt file

--- virustotalscan_ip.py
+++ virustotalscan_ip.py
@@ -1,37 +1,50 @@
-## SCAN IP ADDRESS THRU VIRUSTOTAL API
-## EDITED AND REFINED VERSION BY MRFAEEZ
-## EXERPTED FROM https://www.youtube.com/watch?v=68hHkSRaWZs
+## AUTOMATIC SCAN IP ADDRESS THRU VIRUSTOTAL API
+## BY MRFAEEZ
+## INSPIRED FROM https://www.youtube.com/watch?v=68hHkSRaWZs
 ## if error, probably no results in database
+## usage: python virustotalscan_ip.py <file.txt>
 
-from unittest import result
+## WARNING! ONLY 4 REQUESTS/MIN FOR PUBLIC API ##
+
 import requests
-from bs4 import BeautifulSoup
+import sys
 
-api_key = "<api_key>"
-print("Enter IP Address")
-ip_add = input()
-r = requests.get("https://www.virustotal.com/api/v3/ip_addresses/%s" % ip_add, headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0', 'x-apikey': '%s' %api_key}).json()
+api_key = "<api-key>"
+headers = {"accept": "application/json", "x-apikey": api_key}
+url = "https://www.virustotal.com/api/v3/ip_addresses/"
+contents = []
+n = 0
 
-dict_web = r["data"]["attributes"]["last_analysis_results"]
-tot_engine_c = 0
-tot_detect_c = 0
-result_eng = []
-eng_name = []
-count_harmless = 0
+# READ CONTENT IN FILE
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        contents.append(line)
+        n += 1
 
-for i in dict_web:
-    tot_engine_c = 1 + tot_engine_c
-    if dict_web[i]["category"] == "malicious" or dict_web[i]["category"] == "suspicious":
-        result_eng.append(dict_web[i]["result"])
-        eng_name.append(dict_web[i]["engine_name"])
-        tot_detect_c = 1 + tot_detect_c
-res = []
-for i in result_eng:
-    if i not in res:
-        res.append(i)
-result_eng = res
-if tot_detect_c > 0:
-    print("It is Malicious!")
-    print("The %s was rated for " % ip_add + str(result_eng)[1:-1] + " on " + str(tot_detect_c) + " engines out of " + str(tot_detect_c) + " engines. The Engines which reported this are: " + str(eng_name)[1:-1] + ".")
-else:
-    print("The IP %s " %ip_add + "is clean")
+# START SCANNING WITH API
+for j in range (0, n):
+    r = requests.get(url + contents[j], headers=headers).json()
+
+    # READ JSON RESULT FROM API
+    dict_web = r["data"]["attributes"]["last_analysis_results"]
+    tot_engine_c = 0
+    tot_detect_c = 0
+    result_eng = []
+    count_harmless = 0
+
+    for i in dict_web:
+        tot_engine_c += 1
+        if dict_web[i]["category"] == "malicious" or dict_web[i]["category"] == "suspicious":
+            result_eng.append(dict_web[i]["result"])
+            tot_detect_c += 1
+    res = []
+    for i in result_eng:
+        if i not in res:
+            res.append(i)
+    result_eng = res
+    
+    #PRINT RESULT
+    if tot_detect_c > 0:
+        print("The IP " + contents[j].strip() + " is MALICIOUS and rated as "+ str(result_eng)[1:-1] + " on " + str(tot_detect_c) + " engines out of " + str(tot_detect_c) + " engines.")
+    else:
+        print("The IP " + contents[j].strip() + " is clean")
