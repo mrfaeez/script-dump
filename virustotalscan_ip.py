7c84564d8934715da8f67897f2820e7afa53502f
Faeez <47419302+mrfaeez@users.noreply.github.com>
2022-10-21 10:27:47 +0800

    simplifiy and fix result bug 404
    simplified result and fix bug for when no result found (code 404)

--- virustotalscan_ip.py
+++ virustotalscan_ip.py
@@ -11,7 +11,7 @@ import sys
 
 api_key = "<api-key>"
 headers = {"accept": "application/json", "x-apikey": api_key}
-url = "https://www.virustotal.com/api/v3/ip_addresses/"
+url = "https://www.virustotal.com/api/v3/files/"
 contents = []
 n = 0
 
@@ -23,28 +23,36 @@ with open(sys.argv[1], 'r') as f:
 
 # START SCANNING WITH API
 for j in range (0, n):
-    r = requests.get(url + contents[j], headers=headers).json()
+    r = requests.get(url + contents[j], headers=headers)
 
-    # READ JSON RESULT FROM API
-    dict_web = r["data"]["attributes"]["last_analysis_results"]
-    tot_engine_c = 0
-    tot_detect_c = 0
-    result_eng = []
-    count_harmless = 0
-
-    for i in dict_web:
-        tot_engine_c += 1
-        if dict_web[i]["category"] == "malicious" or dict_web[i]["category"] == "suspicious":
-            result_eng.append(dict_web[i]["result"])
-            tot_detect_c += 1
-    res = []
-    for i in result_eng:
-        if i not in res:
-            res.append(i)
-    result_eng = res
+    if (r.status_code == 404):
+        result = "Result Not Found for IP : "
+        print(result + contents[j].strip())
     
-    #PRINT RESULT
-    if tot_detect_c > 0:
-        print("The IP " + contents[j].strip() + " is MALICIOUS and rated as "+ str(result_eng)[1:-1] + " on " + str(tot_detect_c) + " engines out of " + str(tot_detect_c) + " engines.")
-    else:
-        print("The IP " + contents[j].strip() + " is clean")
+    else: 
+        r = r.json()
+        # READ JSON RESULT FROM API
+        dict_web = r["data"]["attributes"]["last_analysis_results"]
+        tot_engine_c = 0
+        tot_detect_c = 0
+        result_eng = []
+        count_harmless = 0
+
+        for i in dict_web:
+            tot_engine_c += 1
+            if dict_web[i]["category"] == "malicious" or dict_web[i]["category"] == "suspicious":
+                result_eng.append(dict_web[i]["result"])
+                tot_detect_c += 1
+        res = []
+        for i in result_eng:
+            if i not in res:
+                res.append(i)
+        result_eng = res
+    
+        #PRINT RESULT
+        if tot_detect_c > 0:
+            # uncomment line below for detailed result
+            # print("The IP " + contents[j].strip() + " is MALICIOUS and rated as "+ str(result_eng)[1:-1] + " on " + str(tot_detect_c) + " engines out of " + str(tot_detect_c) + " engines.")
+            print("The IP " + contents[j].strip() + " is MALICIOUS from " + str(tot_detect_c) + " engines out of " + str(tot_detect_c) + " engines.")
+        else:
+            print("The IP " + contents[j].strip() + " is clean")
