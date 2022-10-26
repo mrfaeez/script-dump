d4b43a90657b7f94c2cbd1c71d9fde1a0cb204df
Faeez <47419302+mrfaeez@users.noreply.github.com>
2022-10-26 17:05:37 +0800

    simplify, fix 404

--- virustotalscan_hashfile.py
+++ virustotalscan_hashfile.py
@@ -1,7 +1,6 @@
 ## AUTOMATIC SCAN IP ADDRESS THRU VIRUSTOTAL API
 ## BY MRFAEEZ
 ## INSPIRED FROM https://www.youtube.com/watch?v=68hHkSRaWZs
-## if error, probably no results in database
 ## usage: python virustotalscan_hashfile.py <file.txt>
 
 ## WARNING! ONLY 4 REQUESTS/MIN FOR PUBLIC API ##
@@ -23,28 +22,36 @@ with open(sys.argv[1], 'r') as f:
 
 # START SCANNING WITH API
 for j in range (0, n):
-    r = requests.get(url + contents[j], headers=headers).json()
+    r = requests.get(url + contents[j], headers=headers)
 
-    # READ JSON RESULT FROM API
-    dict_web = r["data"]["attributes"]["last_analysis_results"]
-    tot_engine_c = 0
-    tot_detect_c = 0
-    result_eng = []
-    count_harmless = 0
-
-    for i in dict_web:
-        tot_engine_c += 1
-        if dict_web[i]["category"] == "malicious" or dict_web[i]["category"] == "suspicious":
-            result_eng.append(dict_web[i]["result"])
-            tot_detect_c += 1
-    res = []
-    for i in result_eng:
-        if i not in res:
-            res.append(i)
-    result_eng = res
+    if (r.status_code == 404):
+        result = "Result Not Found for File : "
+        print(result + contents[j].strip())
     
-    #PRINT RESULT
-    if tot_detect_c > 0:
-        print("The file " + contents[j].strip() + " is MALICIOUS and rated as "+ str(result_eng)[1:-1] + " on " + str(tot_detect_c) + " engines out of " + str(tot_detect_c) + " engines.")
-    else:
-        print("The file " + contents[j].strip() + " is clean")
+    else: 
+        r = r.json()
+        # READ JSON RESULT FROM API
+        dict_web = r["data"]["attributes"]["last_analysis_results"]
+        tot_engine_c = 0
+        tot_detect_c = 0
+        result_eng = []
+        count_harmless = 0
+
+        for i in dict_web:
+            tot_engine_c += 1
+            if dict_web[i]["category"] == "malicious" or dict_web[i]["category"] == "suspicious":
+                result_eng.append(dict_web[i]["result"])
+                tot_detect_c += 1
+        res = []
+        for i in result_eng:
+            if i not in res:
+                res.append(i)
+        result_eng = res
+    
+        #PRINT RESULT
+        if tot_detect_c > 0:
+            # uncomment line below for detailed result
+            # print("The file " + contents[j].strip() + " is MALICIOUS and rated as "+ str(result_eng)[1:-1] + " on " + str(tot_detect_c) + " engines out of " + str(tot_detect_c) + " engines.")
+            print("The file " + contents[j].strip() + " is MALICIOUS from " + str(tot_detect_c) + " engines out of " + str(tot_detect_c) + " engines.")
+        else:
+            print("The file " + contents[j].strip() + " is clean")
